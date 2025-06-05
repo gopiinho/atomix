9addf8dba5b321fbace7b13f4a0b36a453669a57
Gurpreet <gs109690@gmail.com>
2025-06-05 23:30:28 +0530

    ✨ ERC721 transfer and approval logic

--- src/tokens/ERC721.sol
+++ src/tokens/ERC721.sol
@@ -8,6 +8,14 @@ pragma solidity ^0.8.20;
  */
 abstract contract ERC721 {
     /*////////////////////////////////
+                Errors
+    ////////////////////////////////*/
+    error ERC721__ZeroAddress();
+    error ERC721__NotAuthorized();
+    error ERC721__NotAllowed(address spender);
+    error ERC721__NotOwner(address owner, uint256 tokenId);
+
+    /*////////////////////////////////
                 Events  
     ////////////////////////////////*/
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -27,11 +35,50 @@ abstract contract ERC721 {
     mapping(uint256 => address) public ownerOf;
     mapping(address => uint256) public balanceOf;
 
+    mapping(uint256 => address) public getApproved;
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
     /*////////////////////////////////
                 Functions
     ////////////////////////////////*/
     constructor(string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
+    }
+
+    function approve(address spender, uint256 tokenId) public virtual {
+        address owner = ownerOf[tokenId];
+
+        require(msg.sender == owner, ERC721__NotAllowed(spender));
+
+        getApproved[tokenId] = spender;
+
+        emit Approval(owner, spender, tokenId);
+    }
+
+    function setApprovalForAll(address operator, bool approved) public virtual {
+        isApprovedForAll[msg.sender][operator] = approved;
+
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public virtual {
+        require(from == ownerOf[tokenId], ERC721__NotOwner(from, tokenId));
+        require(to != address(0), ERC721__ZeroAddress());
+        require(
+            msg.sender == from || isApprovedForAll[from][msg.sender] || msg.sender == getApproved[tokenId],
+            ERC721__NotAuthorized()
+        );
+
+        unchecked {
+            balanceOf[from]--;
+            balanceOf[to]++;
+        }
+
+        ownerOf[tokenId] = to;
+
+        delete getApproved[tokenId];
+
+        emit Transfer(from, to, tokenId);
     }
 }
